418677a3a5bdc822c83769150bebbe0bc76fab4d
AizenAngel <boza.mitrovic@gmail.com>
2022-06-23 00:22:53 +0200

    Delete unused data downloader

--- data/get_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $OSTYPE == darwin* ]]; then
-    curl -O http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/sketches_png.zip
-elif [[ $OSTYPE == linux-gnu ]]; then
-    wget http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/sketches_png.zip
-fi
-
-unzip sketches_png.zip && rm sketches_png.zip
